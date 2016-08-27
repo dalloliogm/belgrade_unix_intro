5ebfd8d25253ec004ef8ec997968d746e7ba91dd
Giovanni Dall'Olio <giovanni.m.dallolio@gsk.com>
2016-08-27 03:16:57 -0400

    GEN: moved tutorial text to a separate file

---
+++ src/tutorial_text.py
@@ -0,0 +1,42 @@
+
+
+tutorial_messages = {0 : {'label': 'start', 'file': 'data/exercise1_grep.txt', 'text':"""
+ ______________________________
+/ Congrats!                    \ 
+|  You've used grep correctly, |
+\  and found a cow.            /
+ ------------------------------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\ 
+                ||----w |
+                ||     ||
+
+
+
+    The command grep allows to search for a pattern in a text file.
+    It will print all the matching lines to the screen.
+
+    =================
+    Next Exercise
+    =================
+
+    In the next exercise we will see how to access grep's documentation.
+
+    Grep the following word to continue:
+
+                 | |                  
+               __| |  ___    ___  ___ 
+              / _` | / _ \  / __|/ __|
+             | (_| || (_) || (__ \__ \ 
+              \__,_| \___/  \___||___/
+
+"""},
+    1: {'label': 'docs', 'file': "data/exercise1_grep.txt", 'text':"""
+    grep can be used to 
+    find patterns in a file
+    """
+    }
+    }
+
+
