dd59dab8c5f9fff3971d0218fb30d7769dc7e53b
Giovanni M Dall'Olio <dalloliogm@gmail.com>
2016-09-08 19:16:32 +0100

    AWK: included chr8.gff file, and fixed script

--- src/generate_awk_exercise.R
+++ src/generate_awk_exercise.R
@@ -1,9 +1,11 @@
 # Generate bed files, for Awk exercise
 
 library(Homo.sapiens)
+library(rtracklayer)
 library(dplyr)
 
 chr8 = genes(TxDb.Hsapiens.UCSC.hg19.knownGene) %>% subset(seqnames=='chr8')
-chr8$symbol = AnnotationDbi::select(org.Hs.eg.db, key=chr8$gene_id, keytype='ENTREZID', columns='SYMBOL')$SYMBOL<Paste>
+chr8$symbol = AnnotationDbi::select(org.Hs.eg.db, key=chr8$gene_id, keytype='ENTREZID', columns='SYMBOL')$SYMBOL
 chr8$exerc1 = ifelse(start(chr8)>5000000 & end(chr8) < 10000000, T, F)
 
+export(chr8, "data/genes/chr8.gff", "gff3")
